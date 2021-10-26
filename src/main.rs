8654e98644c496cfdef3472a6469f1072f853244
Peter Fomin <petenmail@rambler.ru>
2021-10-26 23:00:16 +0300

    Trying druid gui

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,71 @@
 use orbital::Vector3D;
 use orbital::lambert::LambertSolver;
 
-fn main() {
-    let mu = 3.986004e5;
-    let r1 = Vector3D {
-        x: 5000.0,
-        y: 10000.0,
-        z: 2100.0
-    };
-    let r2 = Vector3D {
-        x: -14600.0,
-        y: 2500.0,
-        z: 7000.0
-    };
-    let ls = LambertSolver::new(r1, r2, 3600.0, mu);
-    let (v1, v2) = ls.get_velocity();
-    let orbit = ls.get_orbit();
-    println!("{:?}", v1);
-    println!("{:?}", v2);
-    println!("{:?}", orbit);
+use druid::widget::{Align, Flex, Label, TextBox};
+use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WindowDesc, WidgetExt};
 
+const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+const TEXT_BOX_WIDTH: f64 = 200.0;
+const WINDOW_TITLE: LocalizedString<HelloState> = LocalizedString::new("Hello World!");
+
+#[derive(Clone, Data, Lens)]
+struct HelloState {
+    name: String,
 }
+
+fn main() {
+    // describe the main window
+    let main_window = WindowDesc::new(build_root_widget)
+        .title(WINDOW_TITLE)
+        .window_size((400.0, 400.0));
+
+    // create the initial app state
+    let initial_state = HelloState {
+        name: "World".into(),
+    };
+
+    // start the application
+    AppLauncher::with_window(main_window)
+        .launch(initial_state)
+        .expect("Failed to launch application");
+}
+
+fn build_root_widget() -> impl Widget<HelloState> {
+    // a label that will determine its text based on the current app data.
+    let label = Label::new(|data: &HelloState, _env: &Env| format!("Hello {}!", data.name));
+    // a textbox that modifies `name`.
+    let textbox = TextBox::new()
+        .with_placeholder("Who are we greeting?")
+        .fix_width(TEXT_BOX_WIDTH)
+        .lens(HelloState::name);
+
+    // arrange the two widgets vertically, with some padding
+    let layout = Flex::column()
+        .with_child(label)
+        .with_spacer(VERTICAL_WIDGET_SPACING)
+        .with_child(textbox);
+
+    // center the two widgets in the available space
+    Align::centered(layout)
+}
+
+// fn main() {
+//     let mu = 3.986004e5;
+//     let r1 = Vector3D {
+//         x: 5000.0,
+//         y: 10000.0,
+//         z: 2100.0
+//     };
+//     let r2 = Vector3D {
+//         x: -14600.0,
+//         y: 2500.0,
+//         z: 7000.0
+//     };
+//     let ls = LambertSolver::new(r1, r2, 3600.0, mu);
+//     let (v1, v2) = ls.get_velocity();
+//     let orbit = ls.get_orbit();
+//     println!("{:?}", v1);
+//     println!("{:?}", v2);
+//     println!("{:?}", orbit);
+
+// }
